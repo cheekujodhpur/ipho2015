5f551e1ae5e72786847182235d18bc16e17e6c89
sandeshkalantre <sandeshkalantre@bitbucket.org>
2015-01-26 19:09:11 +0530

    Added src_php

---
+++ src_php/header.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"  lang="en-gb"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"  lang="en-gb"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"  lang="en-gb"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<? session_start(); ?>
+
+<html class="" lang="en-gb"> <!--<![endif]-->
+<head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="Homi Bhabha Centre for Science Education, TIFR, IPhO, Olympiads, Science, International Olympiad," />
+        <meta name="author" content="Super User" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="International Physics Olympiad, 2015, Mumbai India" />
+        <title>IPhO 2015</title>
+        
+       
+
+        <link rel="stylesheet" href="./css/14e2e70f726ea65b774ea1909dd9eccc.css" type="text/css" />
+           <style type="text/css">.container{max-width:940px}#sp-header-wrapper{padding: 20px 0 !important; }#sp--wrapper{margin: 10px 0 0 0 !important; }#sp-header-wrapper{padding: 20px 0 !important; }
+                #sp--wrapper{margin: 10px 0 0 0 !important; }#sp-seperatemenu-wrapper{margin: -10px !important; }#sp-main-body-wrapper{background: rgba(246, 180, 74, 0) !important; }#sp-header-wrapper{padding: 20px 0 !important; }
+                #sp--wrapper{margin: 10px 0 0 0 !important; }#sp-seperatemenu-wrapper{margin: -10px !important; }#sp-main-body-wrapper{background: rgba(246, 180, 74, 0) !important; }#sp-footer-wrapper{padding: 30px 0 !important; }
+        </style>
+        
+
+        
+   
+     <!-- <link rel="stylesheet" href="./css/bootstrap-all.css"> -->
+        <!-- Latest compiled and minified JavaScript -->
+
+        <!-- <link rel="stylesheet" href="./bootstrap320/css/bootstrap.css"> -->
+        
+        <link rel="stylesheet" href="./bootstrap320/css/bootstrap-theme.css">
+
+        <link rel="stylesheet" href="./bs3val/css/bootstrapValidator.min.css"/>
+
+         <script type="text/javascript" src="./js/jquery-1.11.1.js"></script> 
+              <!-- <script src="./bootstrap320/js/bootstrap.min.js"></script> -->
+              <script type="text/javascript" src="./bs3val/js/bootstrapValidator.min.js"></script>
+
+
+     <link rel="stylesheet" href="./sb-admin-2/css/bootstrap.css" type="text/css" />   
+
+            <link rel="stylesheet" href="./sb-admin-2/css/sb-admin-2.css" type="text/css" />            
+
+            <link rel="stylesheet" href="./css/custom.css" type="text/css" />
+
+
+<script src="./js/20c45ae36306101aff50c40209bd818d.js" type="text/javascript"></script>
+        <script type="text/javascript">
+                spnoConflict(function($){
+                        function mainmenu() {
+                                $('.sp-menu').spmenu({
+                                        startLevel: 0,
+                                        direction: 'ltr',
+                                        initOffset: {
+                                                x: 0,
+                                                y: 0
+                                        },
+                                        subOffset: {
+                                                x: 0,
+                                                y: 0
+                                        },
+                                        center: 0
+                                });
+                        }
+
+                        mainmenu();
+
+                        $(window).on('resize',function(){
+                                mainmenu();
+                        });
+
+
+                });
+        </script>
+
+
+
+
+
+
+</head>
+
+<?
+// php get_included_files
+include ("./mandb/connect.php");
+include ("./mandb/inc.php");
+
+
+?>
