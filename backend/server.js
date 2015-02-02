b9b4e11c30f8081f0ec563bd52a924454467fc01
Kumar Ayush <cheekujodhpur@gmail.com>
2015-02-02 11:37:20 +0530

    socket.io, auth, user levels

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,47 @@ var express = require('express')
   , app = express()
   , http = require('http')
   , MongoClient = require('mongodb').MongoClient
-  , server = http.createServer(app)
+  , server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
+
+//server running at port 8080
+server.listen(8080);
+console.log('Server running at http://127.0.0.1:8080/');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser());
 
-
 //tell node to send the required files when requested
 app.get('/', function (req, res)
 {
-    res.sendFile(__dirname + '/auth.html');
+		MongoClient.connect("mongodb://localhost:27017/test",function(err,db){
+		if(err)
+		{
+			console.log(err);
+			return 0;
+		}
+		var collection = db.collection('users');
+		collection.find({"ip":req.ip}).toArray(function(err,items)
+		{
+			if(items[0].logged)
+			{
+				var type = items[0].type;
+				if(type)
+				{
+					res.sendFile(__dirname + '/u/index.html');
+				}
+				else
+				{
+					res.sendFile(__dirname + '/su/index.html');
+				}
+			}
+			else
+    			res.sendFile(__dirname + '/auth.html');
+		});
+	});
 });
+
 app.post('/', function(req, res)
 {
 	// Connect to the db
@@ -25,20 +55,66 @@ app.post('/', function(req, res)
 		var collection = db.collection('users');
 		collection.find({"ip":req.ip}).toArray(function(err,items)
 		{
-			var truePass = items[0].pass;
-			if(truePass == req.body.pass)
-				res.sendFile(__dirname + '/index.html');
+			if(items[0].logged)
+			{
+				var type = items[0].type;
+				if(type)
+				{
+					res.sendFile(__dirname + '/u/index.html');
+				}
+				else
+				{
+					res.sendFile(__dirname + '/su/index.html');
+				}
+			}
 			else
-				res.sendFile(__dirname + '/auth.html');
-
+			{
+				var truePass = items[0].pass;
+				if(truePass == req.body.pass)
+				{
+					collection.update({"ip":req.ip},{$set:{"logged":true}},function(err,result){});
+					if(items[0].type)
+						res.sendFile(__dirname + '/u/index.html');
+					else
+						res.sendFile(__dirname + '/su/index.html');
+				}
+				else
+				{
+					res.sendFile(__dirname + '/auth.html');
+				}
+			}
 		});
 	});
 });
-app.get('/index.html', function (req, res)
+
+//logout and redirect to auth
+app.get('/logout.html',function(req,res)
 {
-    res.sendFile(__dirname + '/index.html');
+	MongoClient.connect("mongodb://localhost:27017/test",function(err,db){
+		if(err)
+		{
+			console.log(err);
+			return 0;
+		}
+		var collection = db.collection('users');
+		collection.update({"ip":req.ip},{$set:{"logged":false}},function(err,result){});
+		res.sendFile(__dirname + '/auth.html');
+	});
 });
 
-//server running at port 8080
-app.listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+//io
+io.on('connection',function(socket){
+	console.log('A connection was made');
+	socket.on('setvote',function(body,opt1,opt2,opt3,opt4,time){
+		MongoClient.connect("mongodb://localhost:27017/test",function(err,db){
+		if(err)
+		{
+			console.log(err);
+			return 0;
+		}
+		var collection = db.collection('voteq');
+		collection.insert({"body":body,"opt1":opt1,"opt2":opt2,"opt3":opt3,"opt4":opt4,"time":time},function(err,result){});
+		io.emit('castvote',body,opt1,opt2,opt3,opt4,time);
+		});
+	});
+});
