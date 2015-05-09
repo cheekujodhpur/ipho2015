053b25ccb4bf0abc09679cca952d436fa511f79b
Sandesh Kalantre <kalantresandesh@gmail.com>
2015-05-09 11:05:51 +0530

    updated the shell scripts

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+#create the directories
+rm -r ./uploads
+rm -r ./downloads
+mkdir ./uploads
+mkdir ./downloads
+
+#drop the old databases
+inp1='db.users.drop()'
+echo $inp1
+mongo test --eval $inp1
+
+inp2='db.uploads.drop()'
+echo $inp2
+mongo test --eval $inp2
+#all the ip
 while read -r LINE;do ./add_ip.sh $LINE;done < country_code_ip.txt 
 
