7f12e74a9bec75523d556a01f032c748a5ddfe38
Sandesh Kalantre <kalantresandesh@gmail.com>
2015-06-04 10:53:41 +0530

    getting subparts from file script

---
+++ add_subparts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+read -r TYPE;
+STR="[";
+STR2="[";
+while read -r LINE;
+do 
+    if [[ $LINE != '#'* ]];
+    then
+        set $LINE 
+        STR+="'"$1"'"','; 
+        STR2+="'"$2"'"','; 
+    fi  
+done 
+STR=$STR"]";
+STR2=$STR2"]";
+echo $STR
+echo $STR2
+inp='db.tmp.insert({"type":"'$TYPE'","subparts":'$STR',"maxMarks":'$STR2'})'
+echo $inp
+mongo test --eval $inp
